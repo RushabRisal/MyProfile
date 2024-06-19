30e17432fb0b8ddf220e17f4801a3dc83fbff44e
RushabRisal <rushabofficials@gmail.com>
2024-06-19 10:34:54 +0545

    added transition

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = ({view}) => {
     return ( 
-        <div className={`relative w-[90%] h-[80%] rounded-xl mt-10 shadow-xl bg-[#DAC0A3]/80 backdrop-blur-xl ${view? 'blur-md':'blur-none'}`}>
+        <div className={`relative w-[90%] h-[80%] rounded-xl mt-10 shadow-xl bg-[#DAC0A3]/80 backdrop-blur-xl transition duration-1000 ease-in-out ${view? 'blur-md':'blur-none'}`}>
             <h1 className='absolute w-full flex justify-center md:justify-start items-center top-0 left-0 font-bold p-10 text-4xl'>About Me!</h1>
         </div>
      );
